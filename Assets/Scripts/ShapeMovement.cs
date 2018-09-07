08f8b1224d5a3d4b49209c7409e338acf893fe87
Eddie MacCready <eddiemaccready3@gmail.com>
2018-09-06 17:49:23 -0700

    no message

--- Assets/Scripts/ShapeMovement.cs
+++ Assets/Scripts/ShapeMovement.cs
@@ -16,20 +16,20 @@ public class ShapeMovement : MonoBehaviour {
         {
             RaycastHit2D hitShape = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             
-            if(hitShape.transform.position.y < 0f && hitShape.transform.position.x < 2f && hitShape.transform.gameObject.layer == 11)
+            if(hitShape != null && hitShape.collider != null && hitShape.transform.position.y < 0f && hitShape.transform.position.x < 2f && hitShape.transform.gameObject.layer == 11)
             {
                 zRotationLeft = zRotationLeft + 90;
                 hitShape.transform.rotation = Quaternion.Euler(0f, 0f, zRotationLeft);
 
             }
 
-            else if(hitShape.transform.position.y < 0f && hitShape.transform.position.x > 2f && hitShape.transform.position.x < 5f && hitShape.transform.gameObject.layer == 11)
+            else if(hitShape != null && hitShape.collider != null && hitShape.transform.position.y < 0f && hitShape.transform.position.x > 2f && hitShape.transform.position.x < 5f && hitShape.transform.gameObject.layer == 11)
             {
                 zRotationMiddle = zRotationMiddle + 90;
                 hitShape.transform.rotation = Quaternion.Euler(0f, 0f, zRotationMiddle);
             }
 
-            else if(hitShape.transform.position.y < 0f && hitShape.transform.position.x > 5f && hitShape.transform.gameObject.layer == 11)
+            else if(hitShape != null && hitShape.collider != null && hitShape.transform.position.y < 0f && hitShape.transform.position.x > 5f && hitShape.transform.gameObject.layer == 11)
             {
                 zRotationRight = zRotationRight + 90;
                 hitShape.transform.rotation = Quaternion.Euler(0f, 0f, zRotationRight);
---
+++ Assets/Scripts/ShuffleBlocks.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ShuffleBlocks : MonoBehaviour {
+
+    public int qty;
+    public GameObject[] blocks;
+
+    private int i;
+    
+    public Button shuffleButton;
+    private List<GameObject> blocksGameObjectsList = new List<GameObject>();
+    private List<Vector3> blocksLocationList = new List<Vector3>();
+    private List<Vector3> blocksNewLocationList = new List<Vector3>();
+    private List<Vector3> tempBlocksLocationList = new List<Vector3>();
+    private GameObject[] allGameObjectsArray;
+
+	// Use this for initialization
+	void Start()
+    {
+        Button btn = shuffleButton.GetComponent<Button>();
+
+        //Calls the TaskOnClick/TaskWithParameters method when you click the Button
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        allGameObjectsArray = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        //blocksGameObjectsList = new List<GameObject>();
+        for (int i = 0; i < allGameObjectsArray.Length; i++)
+        {
+            if (allGameObjectsArray[i].layer == 12)
+            {
+                blocksGameObjectsList.Add(allGameObjectsArray[i]);
+                blocksLocationList.Add(allGameObjectsArray[i].transform.position);
+            }
+        }
+
+        tempBlocksLocationList = blocksLocationList;
+
+        foreach (Vector3 item in tempBlocksLocationList)
+        {
+            print("Temp before: " + item.ToString());
+        }
+
+        while(tempBlocksLocationList.Count != 0) 
+        {
+	        int randNum = Random.Range(0, tempBlocksLocationList.Count);
+
+            if (tempBlocksLocationList.Count == 1)
+            {
+                blocksNewLocationList.Add(tempBlocksLocationList[0]);
+                blocksNewLocationList.Remove(tempBlocksLocationList[0]);
+            }
+            else
+            {
+                int tempRandNum = randNum;
+                blocksNewLocationList.Add(tempBlocksLocationList[tempRandNum]);
+                tempBlocksLocationList.Remove(tempBlocksLocationList[tempRandNum]);
+            }
+        }
+
+        foreach (Vector3 item in blocksNewLocationList)
+        {
+            print("New randomized list: " + item.ToString());
+        }
+
+        foreach (Vector3 item in tempBlocksLocationList)
+        {
+            print("Temp after: " + item.ToString());
+        }
+
+        //{
+        //    if (allGameObjectsArray[i].layer == 12)
+        //    {
+        //        blocksGameObjectsList.Add(allGameObjectsArray[i]);
+        //        blocksLocationList.Add(allGameObjectsArray[i].transform.position);
+        //    }
+        //}
+
+        
+
+
+        //int i = Random.Range(0, blocks.Length);
+
+
+        //Debug.Log("You have clicked the button!");
+        //foreach (GameObject item in blocksGameObjectsList)
+        //{
+        //    print(item.ToString());
+        //    Destroy(this.gameObject);
+        //    foreach (Vector3 item in blocksLocationList)
+        //    {
+        //        int i = Random.Range(0, blocks.Length);
+
+        //        // Spawn Block at current Position
+        //        Instantiate(blocks[i],
+        //                    new Vector3(blocksLocationList.x, blocksLocationList.y, blocksLocationList.z),
+        //                    Quaternion.identity);
+        //    }
+        //}
+    }
+}
