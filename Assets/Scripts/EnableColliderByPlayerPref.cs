8fe9bbad57eb8411d7e906f9417bd62c7cc181d3
Eddie MacCready <eddiemaccready3@gmail.com>
2020-02-29 09:26:02 -0800

    - Disabled collider on Buy Now button after purchase is made

---
+++ Assets/Scripts/EnableColliderByPlayerPref.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableColliderByPlayerPref : MonoBehaviour
+{
+    [SerializeField] string thisCityName;
+    private GameSaver gameSaverScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSaverScript = FindObjectOfType<GameSaver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerPrefs.GetInt(thisCityName + gameSaverScript.keyPointsStarEarnedPerLevel) == 1)
+        {
+            transform.gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            transform.gameObject.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+}
